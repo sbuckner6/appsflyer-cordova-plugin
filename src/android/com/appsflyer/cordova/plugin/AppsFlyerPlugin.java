6f2ae9f1be6d7feb7f8bda19f7a6288e1b2df03b
Simon Buckner <sbbucknerv@gmail.com>
2022-03-11 04:21:42 -0500

    update to androidx

--- src/android/com/appsflyer/cordova/plugin/AppsFlyerPlugin.java
+++ src/android/com/appsflyer/cordova/plugin/AppsFlyerPlugin.java
@@ -41,7 +41,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.appsflyer.AppsFlyerConversionListener;
